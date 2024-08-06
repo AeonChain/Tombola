ffce3099592886d5c91f8f2f26b05404597163ec
Thomas Boagey <thomas.boagey@clarity.co.uk>
2024-08-06 22:21:34 +0100

    chore: added prettier, formatted all files

--- solution-1/src/app/Components/List.tsx
+++ solution-1/src/app/Components/List.tsx
@@ -1,30 +1,36 @@
-'use client';
-import Card, { CardData } from './Card';
+"use client";
+import Card, { CardData } from "./Card";
 
 type props = {
-	AvailableBeans?: Bean[];
+  AvailableBeans?: Bean[];
 };
 
 export default function List({ AvailableBeans }: props) {
-	if (!AvailableBeans || !AvailableBeans.length) {
-		return (<div className="w-full flex">
-			<div className='mt-6 w-11/12 text-center mx-auto bg-white'>No beans available? How can this be!?</div>
-		</div>);
-	}
+  if (!AvailableBeans || !AvailableBeans.length) {
+    return (
+      <div className="w-full flex">
+        <div className="mt-6 w-11/12 text-center mx-auto bg-white">
+          No beans available? How can this be!?
+        </div>
+      </div>
+    );
+  }
 
-	const transformBeanDataForCard = (bean: Bean): CardData => {
-		return {
-			title: bean.name, description: bean.description, action: () => {
-				'';
-				console.log(bean.id);
-			}
-		};
-	};
-	return (
-		<div className="w-full flex justify-center mt-6">
-			{(AvailableBeans.map((x, i) => {
-				return <Card key={i} data={transformBeanDataForCard(x)} />;
-			}))}
-		</div>
-	);
-};
+  const transformBeanDataForCard = (bean: Bean): CardData => {
+    return {
+      title: bean.name,
+      description: bean.description,
+      action: () => {
+        ("");
+        console.log(bean.id);
+      },
+    };
+  };
+  return (
+    <div className="w-full flex justify-center mt-6">
+      {AvailableBeans.map((x, i) => {
+        return <Card key={i} data={transformBeanDataForCard(x)} />;
+      })}
+    </div>
+  );
+}
--- solution-1/src/app/Components/icons/FilterIcon.tsx
+++ solution-1/src/app/Components/icons/FilterIcon.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 function FilterIcon(props: React.SVGProps<SVGSVGElement>) {
-	const { height, width, ...html } = props;
-	let heightToUse, widthToUse;
-	if (!height && !width) {
-		heightToUse = 40;
-		widthToUse = 40;
-	} else {
-		heightToUse = height;
-		widthToUse = width;
-	}
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width={widthToUse}
-			height={heightToUse}
-			viewBox="0 0 32 32"
-			{...html}
-		>
-			<path d="M30 6.749H2a1.25 1.25 0 000 2.5h28a1.25 1.25 0 000-2.5zm-6 8.001H8a1.25 1.25 0 000 2.5h16a1.25 1.25 0 000-2.5zm-5 8h-6.053a1.25 1.25 0 000 2.5H19a1.25 1.25 0 000-2.5z"></path>
-		</svg>
-	);
+  const { height, width, ...html } = props;
+  let heightToUse, widthToUse;
+  if (!height && !width) {
+    heightToUse = 40;
+    widthToUse = 40;
+  } else {
+    heightToUse = height;
+    widthToUse = width;
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={widthToUse}
+      height={heightToUse}
+      viewBox="0 0 32 32"
+      {...html}
+    >
+      <path d="M30 6.749H2a1.25 1.25 0 000 2.5h28a1.25 1.25 0 000-2.5zm-6 8.001H8a1.25 1.25 0 000 2.5h16a1.25 1.25 0 000-2.5zm-5 8h-6.053a1.25 1.25 0 000 2.5H19a1.25 1.25 0 000-2.5z"></path>
+    </svg>
+  );
 }
 
 export default FilterIcon;
