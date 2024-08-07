ca0e113d5096bbe03035bf5b024df0d44b9bbbe4
Thomas Boagey <thomas.boagey@clarity.co.uk>
2024-08-07 21:33:52 +0100

    feat: initial work to get the api running

---
+++ solution-2/Controllers/BeanController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace solution_2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BeanController : ControllerBase
+{
+	private readonly ILogger<BeanController> _logger;
+
+	public BeanController(ILogger<BeanController> logger)
+	{
+		_logger = logger;
+	}
+
+	[HttpGet("/all")]
+	public IEnumerable<Beans> All()
+	{
+		this._logger.LogInformation("Get all beans");
+		return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		})
+		.ToArray();
+	}
+
+	[HttpGet("{id}")]
+	public IEnumerable<Beans> Get(Guid id)
+	{
+		this._logger.LogInformation($"Attempting to retrieve bean - {id}");
+		return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		})
+		.ToArray();
+	}
+
+
+}
